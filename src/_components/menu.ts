0951db6c03d7d633c62aeabc624f3e8afa0802e1
zework <florian@hehlen.name>
2025-12-23 01:37:16 +0100

    feat(menu): add styles and dynamic highlighting for active links
    Enhanced the menu component with custom styles using CSS and the `styleMap` directive. Added dynamic background highlighting for active links based on the current path. Improved the visual consistency and user experience.

--- src/_components/menu.ts
+++ src/_components/menu.ts
@@ -1,5 +1,6 @@
-import {html, LitElement} from 'lit'
-import {property, state}  from 'lit/decorators.js'
+import {css, html, LitElement} from 'lit'
+import {property, state}       from 'lit/decorators.js'
+import {styleMap}              from 'lit/directives/style-map.js'
 
 
 
@@ -15,6 +16,16 @@ export class Menu extends LitElement {
   @state()
   data: Items = []
 
+  static override styles = css`
+    wa-tree-item::part(item) {
+      background-color: #88aece;
+    }
+
+    a {
+      background: red;
+    }
+  `
+
   override connectedCallback() {
     super.connectedCallback()
     fetch(this.url)
@@ -26,12 +37,12 @@ export class Menu extends LitElement {
   }
 
   override render() {
-
+    console.log(location.pathname)
     return html`<wa-tree >
                     ${this.data.map(item => html`<wa-tree-item expanded>
-                                                    <a href="${item.data.url}" >${item.data.title}</a>
-                                                    ${item.children?.map(child => html`<wa-tree-item expanded>
-                                                        <a href="${child.data.url}" >${child.data.title}</a>
+                                                    <a href="${item.data.url}" style="${styleMap(location.pathname === item.data.url?{background: '#f0f0f0'}:{})}">${item.data.title}</a>
+                                                    ${item.children?.map(child => html`<wa-tree-item expanded >
+                                                        <a href="${child.data.url}" style="${styleMap(location.pathname === child.data.url?{background: '#f0f0f0'}:{})}">${child.data.title}</a>
                                                     </wa-tree-item>`)}
                                                  </wa-tree-item>`
                     )}
